98a8ab3cb4fdb7cfadcd6589cc6ee9ea2f97863c
Aditya Kumar Kasaudhan <74228301+Aditya8840@users.noreply.github.com>
2024-12-28 07:44:08 +0530

    block: Function for creating block with old block as reference record.

--- types/block.go
+++ types/block.go
@@ -1,9 +1,26 @@
 package types
 
+import (
+	"time"
+
+	"github.com/Aditya8840/GoChain/utils"
+)
+
 type Block struct {
 	Index int
 	Timestamp string
 	Hash []byte
 	Data []byte
 	Prev_hash []byte
+}
+
+func CreateBlock(old *Block, data string) *Block {
+	var block Block
+	currentTime := time.Now()
+	block.Index = old.Index + 1
+	block.Timestamp = currentTime.String()
+	block.Data = []byte(data)
+	block.Prev_hash = old.Hash
+	block.Hash = utils.CalculateHash(block)
+	return &block
 }
